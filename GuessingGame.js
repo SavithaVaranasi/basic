c23feb3a395be39b8556081007e3ae34ad4d5712
Savitha Varanasi <savithavaranasi2002@gmail.com>
2024-07-24 15:50:12 +0530

    js also

---
+++ GuessingGame.js
@@ -0,0 +1,11 @@
+/*var min= 1;
+		// IT MIN IS THE MINIMUM NUMBER IN THE RANGE
+		var max=100;
+		//This max is the maximum number in the range
+		var range= max-min+1;
+		// This is the range where you have to give the number within this range only
+		var number=(Math.floor(Math.random()*range)+min);
+        // HERE Math.floor IS UESD TO ROUND OFF THE DECIMAL NUMBER TO THE NEAREST WHOLE NUMBER
+        console.log(number);*/
+
+		//document.getElementById("rhint").innerHTML = "Opps sorry you loss the game!";
